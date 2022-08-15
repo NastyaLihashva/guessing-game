49dabd2b4b8a9939c76ee113295cfb0b0e088472
NastyaLihashva <kassiopea308@mail.ru>
2022-08-15 17:34:44 +0300

    feat: task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max + this.min)/2);
     }
 
     lower() {
-
+        let num = this.guess();
+        this.max = num;
     }
 
     greater() {
-
+        let num = this.guess();
+        this.min = num;
     }
 }
 
